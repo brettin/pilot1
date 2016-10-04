91f5e1772631d000b12feefca71da7db2bf4cb86
brettin <brettin@anl.gov>
2016-10-04 17:38:05 +0000

    added some argv validation and changed input_dim value

--- scripts/sequential-categorical-class.py
+++ scripts/sequential-categorical-class.py
@@ -13,7 +13,10 @@ from sklearn.cross_validation import KFold
 from sklearn.cross_validation import cross_val_score
 from sklearn.pipeline import Pipeline
 
-
+if (len(sys.argv) != 3):
+	print 'requires arg1=X_fname and arg2=Y_fname'
+	sys.exit(1)
+ 
 X_fname = sys.argv[1] 
 Y_fname = sys.argv[2]
 
@@ -50,16 +53,20 @@ np.random.seed(seed)
 # define baseline model
 def baseline_model():
 
-	# create model
+	# create model: the input_dim of the first layer is the number of features.
+	#               the output_dim of the last layer is the number of classes.
+	#		using sigmoid function in last layer gives probablities 0-1
+	#		and can be used as predicted probablities.
 	model = Sequential()
-	model.add(Dense(4, input_dim=60483, init='normal', activation='relu'))
-	model.add(Dense(2, init='normal', activation='sigmoid'))
+	model.add(Dense(6, input_dim=10, init='normal', activation='relu'))
+	model.add(Dense(4, init='normal', activation='sigmoid'))
 
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 
 
+# create model
 estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
 
 # define the model evaluation procedure to be k-Fold Cross Validation
@@ -68,4 +75,4 @@ kfold = KFold(n=len(X_train), n_folds=10, shuffle=True, random_state=seed)
 
 # evaluate our model (estimator) on our dataset using a 10-fold cross validation
 results = cross_val_score(estimator, X_train, dummy_Y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.sctd()*100))
