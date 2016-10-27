390035f6ab0d6ea8f7286b3e1fb27c3789a03c2f
brettin <brettin@anl.gov>
2016-10-26 20:20:27 -0500

    added dynamic discovery of input_dim and output_dim

--- scripts/read.py
+++ scripts/read.py
@@ -1,13 +1,24 @@
 import numpy as np
+import sys
 
-X_fname = "GDC_downloads/data/ByType.2/X"
-y_fname = "GDC_downloads/data/ByType.2/y"
+if (len(sys.argv) != 3):
+	print 'requires arg1=X_fname and arg2=y_fname'
+	sys.exit(1)
  
+X_fname = sys.argv[1] 
+y_fname = sys.argv[2]
+
 X_train = np.loadtxt(open(X_fname, "rb"), dtype=float, delimiter='\t')
 y_train = np.loadtxt(open(y_fname, "rb"), dtype=int, delimiter='\t')
 
 print X_train.shape
 print y_train.shape
+
+input_dim = X_train.shape[1]
+output_dim = np.unique(y_train).shape[0]
+
+print 'input_dim: ', input_dim
+print 'output_dim: ', output_dim
 
 # this is not needed
 # X = np.array(X_train, dtype=float, copy=False, order='C')
