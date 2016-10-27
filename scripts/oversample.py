2150faf0c82425c233e7be60df1e440177fe6f84
brettin <brettin@anl.gov>
2016-10-26 20:21:36 -0500

    converted to python module

--- scripts/oversample.py
+++ scripts/oversample.py
@@ -3,49 +3,43 @@ from imblearn.over_sampling import ADASYN
 import sys
 import numpy as np
 
-if (len(sys.argv) != 4):
-	print 'requires arg1=X_fname and arg2=Y_fname and bal_strategy'
-	sys.exit(1)
 
-X_fname = sys.argv[1]
-y_fname = sys.argv[2]
-bal_strategy = sys.argv[3]
+def oversample(X_train, y_train, bal_strategy):
 
+	if(bal_strategy == "SMOTESVN"  or bal_strategy == "ALL"):
+		# Apply SMOTE SVM
+		sm = SMOTE(kind='svm')
+		X_resampled, y_resampled = sm.fit_sample(X_train, y_train)
 
-X_train = np.loadtxt(open(X_fname, "rb"), dtype=float, delimiter='\t')
-y_train = np.loadtxt(open(y_fname, "rb"), dtype=int, delimiter='\t')
+		print 'Shape of X_resampled: ', X_resampled.shape
+		print 'Shape of y_resampled: ', y_resampled.shape
 
-print 'Shape of X_train: ', X_train.shape
-print 'Shape of y_Train: ', y_train.shape
+	elif(bal_strategy == "SMOTE"  or bal_strategy == "ALL"):
+		# Apply regular SMOTE
+		sm = SMOTE(kind='regular')
+		X_resampled, y_resampled = sm.fit_sample(X_train, y_train)
 
+		print 'Shape of X_resampled: ', X_resampled.shape
+		print 'Shape of y_resampled: ', y_resampled.shape
 
-
-
-if(bal_strategy == "SMOTESVN"):
-	# Apply SMOTE SVM
-	sm = SMOTE(kind='svm')
-	X_resampled, y_resampled = sm.fit_sample(X_train, y_train)
-
-	print 'Shape of X_resampled: ', X_resampled.shape
-	print 'Shape of y_resampled: ', y_resampled.shape
-
-elif(bal_strategy == "SMOTE"):
-	# Apply regular SMOTE
-	sm = SMOTE(kind='regular')
-	X_resampled, y_resampled = sm.fit_sample(X_train, y_train)
-
-	print 'Shape of X_resampled: ', X_resampled.shape
-	print 'Shape of y_resampled: ', y_resampled.shape
-
-elif(bal_strategy == "ADASYN"):
+	elif(bal_strategy == "ADASYN"  or bal_strategy == "ALL"):
 	# Apply the random over-sampling
-	ada = ADASYN()
-	X_resampled, y_resampled = ada.fit_sample(X_train, y_train)
+		ada = ADASYN()
+		X_resampled, y_resampled = ada.fit_sample(X_train, y_train)
 
-	print 'Shape of X_resampled: ', X_resampled.shape
-	print 'Shape of y_resampled: ', y_resampled.shape
+		print 'Shape of X_resampled: ', X_resampled.shape
+		print 'Shape of y_resampled: ', y_resampled.shape
 
-else:
-	print 'bal_stragegy not in SMOTESVN, SMOTE, ADASYN'
-	sys.exit(1)
+	elif(bal_strategy == 'NONE'):
+		X_resampled = X_train
+		y_resampled = y_train
 
+		print 'Shape of X_resampled: ', X_resampled.shape
+		print 'Shape of y_resampled: ', y_resampled.shape
+
+	else:
+		print 'bal_stragegy not in SMOTESVN, SMOTE, ADASYN, ALL, NONE'
+		sys.exit(1)
+
+
+	return (X_resampled, y_resampled)
