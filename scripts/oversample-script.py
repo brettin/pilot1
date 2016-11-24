ae539ba88442bf1eb2afdc86c53afc115840eb12
brettin <brettin@anl.gov>
2016-11-24 14:05:57 -0600

    convert to functional script

--- scripts/oversample-script.py
+++ scripts/oversample-script.py
@@ -1,7 +1,6 @@
-from imblearn.over_sampling import SMOTE
-from imblearn.over_sampling import ADASYN
 import sys
 import numpy as np
+import oversample as os
 
 if (len(sys.argv) != 4):
 	print 'requires arg1=X_fname and arg2=Y_fname and bal_strategy'
@@ -18,34 +17,11 @@ y_train = np.loadtxt(open(y_fname, "rb"), dtype=int, delimiter='\t')
 print 'Shape of X_train: ', X_train.shape
 print 'Shape of y_Train: ', y_train.shape
 
+X_balanced, y_balanced = os.oversample(X_train, y_train, bal_strategy)
 
+X_balfname = X_fname + "_bal"
+y_balfname = y_fname + "_bal"
 
-
-if(bal_strategy == "SMOTESVN"):
-	# Apply SMOTE SVM
-	sm = SMOTE(kind='svm')
-	X_resampled, y_resampled = sm.fit_sample(X_train, y_train)
-
-	print 'Shape of X_resampled: ', X_resampled.shape
-	print 'Shape of y_resampled: ', y_resampled.shape
-
-elif(bal_strategy == "SMOTE"):
-	# Apply regular SMOTE
-	sm = SMOTE(kind='regular')
-	X_resampled, y_resampled = sm.fit_sample(X_train, y_train)
-
-	print 'Shape of X_resampled: ', X_resampled.shape
-	print 'Shape of y_resampled: ', y_resampled.shape
-
-elif(bal_strategy == "ADASYN"):
-	# Apply the random over-sampling
-	ada = ADASYN()
-	X_resampled, y_resampled = ada.fit_sample(X_train, y_train)
-
-	print 'Shape of X_resampled: ', X_resampled.shape
-	print 'Shape of y_resampled: ', y_resampled.shape
-
-else:
-	print 'bal_stragegy not in SMOTESVN, SMOTE, ADASYN'
-	sys.exit(1)
+np.savetxt( X_balfname, X_balanced, fmt="%10.2f", delimiter='\t')
+np.savetxt( y_balfname, y_balanced, fmt="%s", delimiter='\t')
 
