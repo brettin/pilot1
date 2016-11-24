79b119325f296fcb59d4a12aa97ea87a15863273
brettin <brettin@anl.gov>
2016-11-23 18:56:17 -0600

    parameterized dropout rate

--- scripts/undersample-test.py
+++ scripts/undersample-test.py
@@ -17,14 +17,15 @@ from sklearn.model_selection import train_test_split
 
 # muck with command line args 
 if (len(sys.argv) < 4):
-	print 'requires arg1=X_fname and arg2=Y_fname and bal_strategy'
+	print 'requires arg1=X_fname and arg2=Y_fname and bal_strategy lr_rate'
 	sys.exit(1)
 
 X_fname      = sys.argv[1]
 y_fname      = sys.argv[2]
 bal_strategy = sys.argv[3]
 learn_rate   = sys.argv[4]
-# optimizer   = sys.argv[5]
+dropout      = sys.argv[5]
+# optimizer   = sys.argv[6]
 nb_epoch=50
 
 
@@ -34,6 +35,7 @@ print "Using ", y_fname, " as labels"
 print "Using ", bal_strategy, " as undersample strategy"
 print "Using ", learn_rate, " as the learning rate"
 print "Using ", nb_epoch, " as the number of epochs"
+print "Using ", dropout, " as the dropout rate"
 
 X = np.loadtxt(open(X_fname, "rb"), dtype=float, delimiter='\t')
 y = np.loadtxt(open(y_fname, "rb"), dtype=int, delimiter='\t')
@@ -76,12 +78,15 @@ print 'input_dim: ', input_dim
 print 'output_dim: ', output_dim
 
 # define baseline model
-def baseline_model(opt, output_dim):
+def baseline_model(opt=SGD(lr=learn_rate), output_dim=0, input_dim=0, dropout=.1):
+	print "Using ", learn_rate, " as the learning rate"
+	print "Using ", dropout, " as the dropout rate"
+
 	model = Sequential()
 	model.add(Dense(2000, input_dim=input_dim, init='normal', activation='sigmoid'))
-	model.add(Dropout(.10))
+	model.add(Dropout(dropout))
 	model.add(Dense(200, init='normal', activation='sigmoid'))
-	model.add(Dropout(.10))
+	model.add(Dropout(dropout))
 	model.add(Dense(output_dim, init='normal', activation='sigmoid'))
 	
 	# Compile model
@@ -89,8 +94,24 @@ def baseline_model(opt, output_dim):
 	return model
 
 
-baseline_model = baseline_model(optimizer, output_dim)
+baseline_model = baseline_model(optimizer, output_dim, input_dim, dropout)
 baseline_model.fit(X_train, y_onehot, nb_epoch=nb_epoch, batch_size=32, verbose=1)
 scores = baseline_model.evaluate(X_test, y_test_onehot)
 print("%s: %.2f%%" % (baseline_model.metrics_names[1], scores[1]*100))
+
+
+# define the model evaluation procedure to be k-Fold Cross Validation
+# estimator = KerasClassifier(build_fn=baseline_model, opt=optimizer, output_dim=output_dim, nb_epoch=nb_epoch, batch_size=32, verbose=0)
+
+# evaluate our model (estimator) on our dataset using a 10-fold cross validation
+# set the number of folds to be 10 and to shuffle the data before partitioning it.
+
+# fix random seed for reproducibility
+# seed = 7
+# np.random.seed(seed)
+
+
+# kfold = KFold(n=len(X_train), n_folds=2, shuffle=True, random_state=seed)
+# results = cross_val_score(estimator, X_train, y_onehot, cv=kfold)
+# print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.sctd()*100))
 
